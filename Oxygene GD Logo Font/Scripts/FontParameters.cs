e7928f7d6e882c9f7002d9365bb97376abe05cea
AmaliJD <60673543+AmaliJD@users.noreply.github.com>
2021-12-19 22:50:15 -0500

    highlight align edit

--- Oxygene GD Logo Font/Scripts/FontParameters.cs
+++ Oxygene GD Logo Font/Scripts/FontParameters.cs
@@ -11,20 +11,22 @@ public class FontParameters : MonoBehaviour
 {
     public InputField input;
     public Toggle allCaps;
-    public TextMeshPro text_bg, text_highlight, text_main, text_secondary, text_under;
+    public TextMeshPro text_bg, text_highlight, text_highlight2, text_main, text_secondary, text_under;
 
     public ColorPicker main_color, secondary_color, under_color, highlight_color, border_color, outline_color, shadow_color;
     public Dropdown colorTypeDropdown;
 
-    public Slider outline_slider, border_slider, shade_slider, shadow_slider, blur_slider, x_slider, y_slider, hue_slider, sat_slider;
-    public Text outlineVal, borderVal, shadeVal, shadowVal, blurVal, xVal, yVal, hueVal, satVal;
+    public Slider outline_slider, border_slider, shade_slider, shadow_slider, blur_slider, x_slider, y_slider, hue_slider, sat_slider, highlight_slider;
+    public Text outlineVal, borderVal, shadeVal, shadowVal, blurVal, xVal, yVal, hueVal, satVal, highVal;
 
     public VolumeProfile volume;
     private ColorAdjustments colorAdj;
 
     private Color initMain, initSec, initUnder, initHigh, initBor, initOut, initShad;
-    private float initOutVal, initBorVal, initShadeVal, initShadVal, initBlurVal, initXVal, initYVal;
+    private float initOutVal, initBorVal, initShadeVal, initShadVal, initBlurVal, initXVal, initYVal, initHighVal;
     private int initHueVal, initSatVal;
+
+    private int highlightAlign;
 
     public TransparentBackgroundScreenshotRecorder BGCapture;
 
@@ -52,6 +54,7 @@ public class FontParameters : MonoBehaviour
         initYVal = y_slider.value;
         initHueVal = (int)hue_slider.value;
         initSatVal = (int)sat_slider.value;
+        initHighVal = highlight_slider.value;
 
         if (!volume.TryGet(out colorAdj)) throw new System.NullReferenceException(nameof(colorAdj));
     }
@@ -61,6 +64,7 @@ public class FontParameters : MonoBehaviour
         text = allCaps.isOn ? input.text.ToUpper() : input.text;
         text_bg.text = text;
         text_highlight.text = text;
+        text_highlight2.text = text;
         text_main.text = text;
         text_secondary.text = text;
         text_under.text = text;
@@ -71,6 +75,7 @@ public class FontParameters : MonoBehaviour
         text_secondary.GetComponent<TMP_Text>().outlineColor = border_color.CurrentColor;
         text_under.fontSharedMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, under_color.CurrentColor);
         text_highlight.fontSharedMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, highlight_color.CurrentColor);
+        text_highlight2.fontSharedMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, highlight_color.CurrentColor);
         text_bg.fontSharedMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, shadow_color.CurrentColor);
         text_bg.GetComponent<TMP_Text>().outlineColor = outline_color.CurrentColor;
 
@@ -86,6 +91,13 @@ public class FontParameters : MonoBehaviour
         text_bg.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlaySoftness, blur_slider.value / 100f);
         text_under.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetY, shade_slider.value / 100f);
 
+        text_highlight.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayDilate, -1 * (highlight_slider.value * 2 / 100));
+        text_highlight.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetX, ((highlightAlign == 0 || highlightAlign == 2) ? 1 : -1) * (highlight_slider.value * 2 / 100));
+        text_highlight.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetY, ((highlightAlign == 2 || highlightAlign == 3) ? 1 : -1) * (highlight_slider.value * 2 / 100));
+        text_highlight2.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayDilate, -1 * (highlight_slider.value * 2 / 100));
+        text_highlight2.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetX, ((highlightAlign == 0 || highlightAlign == 2) ? 1 : -1) * (highlight_slider.value * 2 / 100));
+        text_highlight2.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetY, ((highlightAlign == 2 || highlightAlign == 3) ? 1 : -1) * (highlight_slider.value * 2 / 100));
+
         colorAdj.hueShift.Override(hue_slider.value);
         colorAdj.saturation.Override(sat_slider.value);
 
@@ -98,6 +110,7 @@ public class FontParameters : MonoBehaviour
         yVal.text = "Y: " + y_slider.value;
         hueVal.text = "Hue: " + (hue_slider.value >= 0 ? "+" : "") + hue_slider.value;
         satVal.text = "Saturation: " + (sat_slider.value >= 0 ? "+" : "") + sat_slider.value;
+        highVal.text = "Highlight: " + highlight_slider.value;
 
         main_color.gameObject.SetActive(colorTypeDropdown.value == 0);
         secondary_color.gameObject.SetActive(colorTypeDropdown.value == 1);
@@ -117,8 +130,12 @@ public class FontParameters : MonoBehaviour
         }
     }
 
-    //private Color initMain, initSec, initUnder, initHigh, initBor, initOut, initShad;
-    //private float initOutVal, initBorVal, initShadVal, initBlurVal, initXVal, initYVal;
+    public void setHighlightAlign(int i)
+    {
+        highlightAlign = i;
+        text_highlight.gameObject.SetActive(highlightAlign == 0 || highlightAlign == 1);
+        text_highlight2.gameObject.SetActive(highlightAlign == 2 || highlightAlign == 3);
+    }
 
     public void Reset()
     {
@@ -138,6 +155,10 @@ public class FontParameters : MonoBehaviour
         border_slider.value = initBorVal;
         hue_slider.value = initHueVal;
         sat_slider.value = initSatVal;
+        highlight_slider.value = initHighVal;
+        highlightAlign = 0;
+        text_highlight.gameObject.SetActive(true);
+        text_highlight2.gameObject.SetActive(false);
     }
 
     private void OnApplicationQuit()
@@ -150,6 +171,7 @@ public class FontParameters : MonoBehaviour
         text_secondary.GetComponent<TMP_Text>().outlineColor = border_color.CurrentColor;
         text_under.fontSharedMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, under_color.CurrentColor);
         text_highlight.fontSharedMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, highlight_color.CurrentColor);
+        text_highlight2.fontSharedMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, highlight_color.CurrentColor);
         text_bg.fontSharedMaterial.SetColor(ShaderUtilities.ID_UnderlayColor, shadow_color.CurrentColor);
         text_bg.GetComponent<TMP_Text>().outlineColor = outline_color.CurrentColor;
 
@@ -164,6 +186,13 @@ public class FontParameters : MonoBehaviour
         text_bg.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetY, y_slider.value / 10f);
         text_bg.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlaySoftness, blur_slider.value / 100f);
         text_under.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetY, shade_slider.value / 100f);
+
+        text_highlight.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayDilate, -1 * (highlight_slider.value * 2 / 100));
+        text_highlight.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetX, ((highlightAlign == 0 || highlightAlign == 2) ? 1 : -1) * (highlight_slider.value * 2 / 100));
+        text_highlight.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetY, ((highlightAlign == 2 || highlightAlign == 3) ? 1 : -1) * (highlight_slider.value * 2 / 100));
+        text_highlight2.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayDilate, -1 * (highlight_slider.value * 2 / 100));
+        text_highlight2.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetX, ((highlightAlign == 0 || highlightAlign == 2) ? 1 : -1) * (highlight_slider.value * 2 / 100));
+        text_highlight2.fontSharedMaterial.SetFloat(ShaderUtilities.ID_UnderlayOffsetY, ((highlightAlign == 2 || highlightAlign == 3) ? 1 : -1) * (highlight_slider.value * 2 / 100));
 
         colorAdj.hueShift.Override(hue_slider.value);
         colorAdj.saturation.Override(sat_slider.value);
